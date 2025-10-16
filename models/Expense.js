10b09ffd4daab9f9be9ffb2d4dbbc41391d76b61
Rohitash Singh <rohitashmtr2002@gmail.com>
2025-10-16 21:07:17 +0530

    Refactor Expense schema formatting and add createdBy field

--- models/Expense.js
+++ models/Expense.js
@@ -1,15 +1,25 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 const ExpenseSchema = new mongoose.Schema(
   {
-    group: { type: mongoose.Schema.Types.ObjectId, ref: 'Group', required: true },
+    group: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Group",
+      required: true,
+    },
     title: { type: String, required: true, trim: true },
     amount: { type: Number, required: true, min: 0 },
-    paidBy: { type: String, required: true },      // member subdoc _id as string
-    participants: { type: [String], default: [] },  // array of member subdoc _id strings
+    paidBy: { type: String, required: true }, // member subdoc _id as string
+    participants: { type: [String], default: [] }, // array of member subdoc _id strings
+    createdBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: false,
+      index: true,
+    },
   },
   { timestamps: true }
 );
 
-const Expense = mongoose.model('Expense', ExpenseSchema);
+const Expense = mongoose.model("Expense", ExpenseSchema);
 export default Expense;
