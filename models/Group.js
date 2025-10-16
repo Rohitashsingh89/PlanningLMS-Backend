b5125d04a353486588e45f62feb26ecb88744a49
Rohitash Singh <rohitashmtr2002@gmail.com>
2025-10-16 21:06:57 +0530

    Fix string quotes in Group.js

--- models/Group.js
+++ models/Group.js
@@ -1,4 +1,4 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 const MemberSchema = new mongoose.Schema(
   {
@@ -10,10 +10,16 @@ const MemberSchema = new mongoose.Schema(
 const GroupSchema = new mongoose.Schema(
   {
     name: { type: String, required: true, trim: true },
+    owner: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: false,
+      index: true,
+    },
     members: { type: [MemberSchema], default: [] },
   },
   { timestamps: true }
 );
 
-const Group = mongoose.model('Group', GroupSchema);
+const Group = mongoose.model("Group", GroupSchema);
 export default Group;
