64609bfa556f5ea86872a136fa494c89d45f7f71
Rohitash Singh <rohitashmtr2002@gmail.com>
2025-10-16 21:07:41 +0530

    Enhance post schema with owner and isComplete
    Added owner and isComplete fields to post schema.

--- models/post.js
+++ models/post.js
@@ -1,16 +1,27 @@
-import mongoose, { mongo } from 'mongoose'
+import mongoose, { mongo } from "mongoose";
 
 const post = new mongoose.Schema({
-    title: {
-        type: String,
-        required: true
-    },
-    description: {
-        type: String,
-        required: true
-    }
-})
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: false,
+    index: true,
+  },
+  isComplete: {
+    type: Boolean,
+    default: false,
+    default: false,
+  },
+});
 
-const Post = mongoose.model('post', post);
+const Post = mongoose.model("post", post);
 
 export default Post;
