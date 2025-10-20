4b895140c44eba072514cdfbd289f90111dacfe2
Rohitash Singh <rohitashmtr2002@gmail.com>
2025-10-20 11:10:42 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -32,7 +32,7 @@ const DATABASE_NAME = process.env.DATABASE_NAME || "test"; // Default if missing
 // app.use(cors());
 app.use(
   cors({
-    origin: ["https://codechisel-official.vercel.app", "http://localhost:3000", "https://diwali-crazy.vercel.app"],
+    origin: ["https://codechisel-official.vercel.app", "http://localhost:3000", "https://diwali-crazy.vercel.app", "http://localhost:5173"],
     credentials: true,
   })
 );
